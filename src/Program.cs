dd1eaf7ea4f7bfd649b0cc768fb30df3051aae50
the-only-huy <thehauntedspeechkeeper>
2024-02-05 17:46:21 +0700

    initialize project

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using BlazorHealthApp.Shared;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorHealthApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault();
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton<IModel, MainModel>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/Shared/TargetHeartRate.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorHealthApp.Shared
+{
+    public class TargetHeartRate
+    {
+        public int Low { get; set; }
+        public int High { get; set; }
+    }
+}
